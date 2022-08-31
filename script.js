36f67d54331678b8019a9eca2132a68c8dc0949b
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2022-08-31 21:48:23 +0300

    add styles to Contact Us section

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
 'use strict';
-// ------- Hamburger menu ------------
+// -------Variables-------------------
 const menuBtn = document.querySelector('.nav__menu');
 const hamburger = document.querySelector('.nav__burger');
 const menu = document.querySelector('.nav__links');
 const mobLogo = document.querySelector('.mobile-logo');
 const links = document.querySelectorAll('.nav__link');
 let showMenu = false;
+// ------- Hamburger menu ------------
+
+function openModalWindow() {
+  hamburger.classList.add('open');
+  menu.classList.add('open');
+  mobLogo.classList.add('hidden');
+  document.querySelector('body').classList.add('prevent-scroll');
+  links.forEach((i) => {
+    i.classList.add('open');
+    i.addEventListener('click', () => {
+      hamburger.classList.remove('open');
+      menu.classList.remove('open');
+      mobLogo.classList.remove('hidden');
+      document.querySelector('body').classList.remove('prevent-scroll');
+      links.forEach((i) => {
+        i.classList.remove('open');
+      });
+      showMenu = false;
+    });
+  });
+  showMenu = true;
+}
+function closeModalWindow() {
+  hamburger.classList.remove('open');
+  menu.classList.remove('open');
+  mobLogo.classList.remove('hidden');
+  document.querySelector('body').classList.remove('prevent-scroll');
+  links.forEach((i) => {
+    i.classList.remove('open');
+  });
+  showMenu = false;
+}
 menuBtn.addEventListener('click', () => {
   if (!showMenu) {
-    hamburger.classList.add('open');
-    menu.classList.add('open');
-    mobLogo.classList.add('hidden');
-    document.querySelector('body').classList.add('prevent-scroll');
-    links.forEach((i) => {
-      i.classList.add('open');
-      i.addEventListener('click', () => {
-        hamburger.classList.remove('open');
-        menu.classList.remove('open');
-        mobLogo.classList.remove('hidden');
-        document.querySelector('body').classList.remove('prevent-scroll');
-
-        links.forEach((i) => {
-          i.classList.remove('open');
-        });
-        showMenu = false;
-      });
-    });
-    showMenu = true;
+    openModalWindow();
   } else {
-    hamburger.classList.remove('open');
-    menu.classList.remove('open');
-    mobLogo.classList.remove('hidden');
-    document.querySelector('body').classList.remove('prevent-scroll');
-
-    links.forEach((i) => {
-      i.classList.remove('open');
-    });
-    showMenu = false;
+    closeModalWindow();
   }
 });
+// --------Subscribe btn------
